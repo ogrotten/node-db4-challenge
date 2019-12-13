3b14c8911c2aada21392c91739947647e6a11cd1
Darrin Lowery <lambda@monsto.com>
2019-12-12 19:39:24 -0600

    fix seed errors

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -5,9 +5,9 @@ exports.seed = function (knex) {
 		.then(function () {
 			// Inserts seed entries
 			return knex('recipes').insert([
-				{ id: 1, name: 'Cheese', text: "Individually wrapped." },
-				{ id: 2, name: 'Kool Aid', text: "Drink it." },
-				{ id: 3, name: 'Pop Tarts', text: "Need more fruit." }
+				{ id: 1, name: 'Cheese'},
+				{ id: 2, name: 'Kool Aid'},
+				{ id: 3, name: 'Pop Tarts' }
 			]);
 		});
 };
